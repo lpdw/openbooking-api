676566168a9cdae18e35b3fa369e11fb0cec441e
Laouiti Elias Cédric <elias@laouiti.me>
2015-12-18 17:58:03 +0100

    Ajout de la structure de bdd Ajout fichier .htaccess pour rediriger toutes les requetes vers index.php Ajout du dossier d'installation de l'API

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: laouitielias
+ * Date: 18/12/2015
+ * Time: 16:42
+ */
+
+   //Todo : Routeur pour chacun des appels à l'API
+
+//    $action = $_REQUEST
---
+++ installation/index.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: laouitielias
+ * Date: 18/12/2015
+ * Time: 16:40
+ */
+
+// Todo : Script de création de la table en bdd (Table générée à partir du fichier SQL/DBSchema.mwb => MysqlWorkbench file)
