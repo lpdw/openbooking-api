377a3497430794216087da3e1bffb81f3b2976d2
Maxime Florile <florile.maxime@gmail.com>
2016-01-05 17:05:14 +0100

    Class Event and ModelEvent, =>PHPDoc fixes

--- _class/model/ModelEvent.php
+++ _class/model/ModelEvent.php
@@ -10,22 +10,76 @@
  */
 
 namespace OpenBooking\model;
+
 /**
  * Class ModelEvent
- * @package OpenBooking\model
+ * @package OpenBooking\Model\ModelParticpant
  */
-
 Class ModelEvent
 {
+    /**
+     * Event ID
+     * @var int
+     */
     public $id;
+
+    /**
+     * Event name
+     * @var string
+     */
     public $name;
+
+    /**
+     * Event description
+     * @var string
+     */
     public $description;
+
+    /**
+     * Event localisation
+     * @var string
+     */
     public $localisation;
+
+    /**
+     * Event date
+     * @var int Timestamp required
+     */
     public $date;
+
+    /**
+     * Max participants allowed for the Event
+     * @var int
+     */
     public $participants_max;
+
+    /**
+     * Event organizer
+     * @var string
+     */
     public $organizer;
+
+    /**
+     * Email address of Event organizer
+     * @var string
+     */
     public $organizer_email;
+
+    /**
+     * Event creation date
+     * @var int Timestamp required
+     */
     public $creation_date;
+
+    /**
+     * Event registration status (1: opened or 0: closed)
+     * @var int
+     */
     public $open_to_registration;
+
+    /**
+     * Event status (1: cancelled or 0: not cancelled)
+     * @var int
+     */
     public $cancelled;
 }
