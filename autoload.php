07c46637d2ad8f81a6bea5e67e873132037ca9ba
Laouiti Elias Cédric <elias@laouiti.me>
2016-01-08 10:14:37 +0100

    Add autoload

---
+++ autoload.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Autoload file.
+ * @version 1.0
+ * @author Elias Cédric Laouiti
+ * @author Maxime Florile
+ * Date: 04/01/2016
+ * Project: OpenBooking
+ * @copyright 2015 - 2016 OpenBooking Group
+ */
+
+require_once dirname(__FILE__)."/_class/metier/Participant.php";
+require_once dirname(__FILE__)."/_class/metier/Participation.php";
+require_once dirname(__FILE__)."/_class/metier/Event.php";
+require_once dirname(__FILE__)."/_class/metier/Email.php";
+require_once dirname(__FILE__)."/_class/metier/EmailType.php";
